c4c559b3e155700bbd8cc5455f710a91de6659ee
Vlad Krasovski <krasovsky@kentike.com>
2026-02-21 19:45:28 +0800

    weather tool

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,13 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router';
 
-import { Chat } from '../components/Chat'
+import { Chat } from '../components/Chat';
 
-export const Route = createFileRoute('/')({ component: App })
+export const Route = createFileRoute('/')({ component: App });
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100">
       <Chat />
     </div>
-  )
+  );
 }
