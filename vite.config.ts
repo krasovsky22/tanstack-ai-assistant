3152cb031f2e49647d5410cfe86a6a5710f64632
Vlad Krasovski <krasovsky@kentike.com>
2026-03-04 19:07:55 +0800

    allow docker host

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,30 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vite';
+import { devtools } from '@tanstack/devtools-vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import { nitro } from 'nitro/vite'
+import viteReact from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import { nitro } from 'nitro/vite';
 
 const config = defineConfig({
   server: {
     host: process.env.HOST || 'localhost',
+    allowedHosts: ['host.docker.internal'],
   },
   plugins: [
     devtools(),
-    nitro({ rollupConfig: { external: [/^@sentry\//, 'postgres', /^drizzle-orm/, 'node-cron'] } }),
+    nitro({
+      rollupConfig: {
+        external: [/^@sentry\//, 'postgres', /^drizzle-orm/, 'node-cron'],
+      },
+    }),
     tsconfigPaths({ projects: ['./tsconfig.json'] }),
     tailwindcss(),
     tanstackStart(),
     viteReact(),
   ],
-})
+});
 
-export default config
+export default config;
